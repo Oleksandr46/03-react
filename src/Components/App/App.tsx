4002a6439744caaab5b1410b39301c14c1b5124c
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2026-05-03 17:05:36 +0200

    fix''

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,23 +2,24 @@ import { useState } from 'react';
 import type { Article } from '../../types/article';
 import ArticleList from '../ArticleList/ArticleList';
 import SearchForm from '../SearchForm/SearchForm';
-import { fetchArticles } from "../../services/ArticleService";
+import { fetchArticles } from '../../services/ArticleService';
 
 
 
 
 export default function App() {
     const [articles, setArticles] = useState<Article[]>([]);
-    const [isLonding, setIsLonding] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     // 1. Оголошуємо стан
     const [isError, setIsError] = useState(false);
 
     const handleSearch = async (topic: string) => {
         // 2. Додаємо блок try...catch
         try {
-            setIsLonding(true)
+            setIsLoading(true)
              // 3. Скидаємо стан помилки в false перед кожним запитом
             setIsError(false)
+            setArticles([]);
             const data = await fetchArticles(topic);
             setArticles(data)
         } catch {
@@ -27,14 +28,14 @@ export default function App() {
         } finally {
             // 5. Встановлюємо стан isLoading в false
       // після будь якого результату запиту
-            setIsLonding(false);
+            setIsLoading(false);
         }
   };
 
   return (
     <div>
           <SearchForm onSubmit={handleSearch} />
-          {isLonding && <p>Lopding data, please wait ...</p>}
+          {isLoading && <p>Loading data, please wait ...</p>}
           {isError && <p>Whoops, something went wrong! Please try again!</p>}
       {articles.length > 0 && <ArticleList items={articles} />}
     </div>
