71be47aca0ef6b7ecc28009f5934551979c32676
Oleksandr Melnyk <melnykoleksandr724@gmail.com>
2026-05-08 19:13:12 +0200

    03-react

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,43 +1,203 @@
-import { useState } from 'react';
-import type { Article } from '../../types/article';
-import ArticleList from '../ArticleList/ArticleList';
-import SearchForm from '../SearchForm/SearchForm';
-import { fetchArticles } from '../../services/articleService';
+import { useEffect, useState } from 'react';
+// import type { Article } from '../../types/article';
+// import ArticleList from '../ArticleList/ArticleList';
+// import SearchForm from '../SearchForm/SearchForm';
+// import { fetchArticles } from '../../services/articleService';
+// import OrderForm from '../OrderForm/OrderForm';
+// import axios from 'axios';
+// import Timer from '../Timer/Timer';
+
+// import Modal from "../Modal/Modal";
 
 
 
+// export default function App() {
+    // const [count, setCount] = useState(1);
+    // const [clicks, setClicks] = useState(1);
+    // const [person, setPerson] = useState(null);
+    // useEffect(() => {
+    //     console.log("Effect ran!");
+    //     console.log("Clicks updated:", count);
+    //     useEffect(() => {
+    //         async function fetchCharacter() {
+    //             // You can await here
+    //             const response = await axios.get(`https://swapi.info/api/people/${clicks}`);
+    //             setPerson(response.data);
+    //         }
+    // // ...
+    //   fetchCharacter();
+    // }, [clicks]);
+    // console.log('App rendred!');
+//     useEffect(() => {
+//     console.log("You can see me only once!");
+//   }, []);
+    
+    // useEffect(() => {
+    //     console.log("Clicks updated:", clicks);
+    // }, [clicks]);
+// return (
+//     <>
+//         {/* <h2>The count is {count}</h2>
+//         {/* <button onClick={() => setCount(count + 1)}>Get next character</button> */}
+//         {/* <button onClick={() => setClicks(clicks + 1)}>You clicked {clicks} times</button> */}
+        
+//         {/* <pre>{JSON.stringify(person, null, 2)}</pre> */} */}
+// </>
+// );
+//==========================================================================
+//     const [articles, setArticles] = useState<Article[]>([]);
+//     const [isLoading, setIsLoading] = useState(false);
+//     // 1. Оголошуємо стан
+//     const [isError, setIsError] = useState(false);
+
+//     const handleSearch = async (topic: string) => {
+//         // 2. Додаємо блок try...catch
+//         try {
+//             setIsLoading(true)
+//              // 3. Скидаємо стан помилки в false перед кожним запитом
+//             setIsError(false)
+//             setArticles([]);
+//             const data = await fetchArticles(topic);
+//             setArticles(data)
+//         } catch {
+//             // 4. Встановлюємо стан isError в true
+//             setIsError(true);
+//         } finally {
+//             // 5. Встановлюємо стан isLoading в false
+//       // після будь якого результату запиту
+//             setIsLoading(false);
+//         }
+//   };
+
+//   return (
+//     <div>
+//           <SearchForm onSubmit={handleSearch} />
+//           {isLoading && <p>Loading data, please wait ...</p>}
+//           {isError && <p>Whoops, something went wrong! Please try again!</p>}
+//           {articles.length > 0 && <ArticleList items={articles} />}
+//           <OrderForm/>
+//     </div>
+//       );
+//========================================================================================
+// }
+
+
+// export default function App() {
+//     const [count, setCount] = useState(0);
+//     // const [isOpen, setIsOpen] = useState(false);
+//     useEffect(() => {
+//         console.log(`Effect ran for: ${count}`);
+//         // Повертаємо функцію очищення ефекта
+//         return () => {
+//             console.log(`Clean up for ${count}`);
+//         };
+//     }, [count])
+//   return (
+//       <>
+//           <button onClick={()=> setCount(count + 1)}>Count is {count}</button>
+//     </>
+//   );
+// }
+//========================================================================================
+
+// export default function App() {
+//     const [isOpen, setIsOpen] = useState(false);
+    
+//   return (
+//       <>
+//           <button onClick={() => setIsOpen(!isOpen)}>{isOpen ? `Hide timer` : `Show timer`}</button>
+//           {isOpen && <Timer />}</>
+//   );
+// }
+//========================================================================================
+// export default function App() {
+//     const [isModalOpen, setIsModalOpen] = useState(false);
+//     const openModal = () => setIsModalOpen(true);
+//     const closeModal = () => setIsModalOpen(false);
+
+//   return (
+//       <div>
+//           <h1>Main content of the page</h1>
+//           <button onClick={openModal}>Open modal</button>
+//           {isModalOpen && <Modal onClose={closeModal}>
+//             <h2>Custom Modal Content</h2>
+//               <p>This is a reusable modal with dynamic content.</p>
+//           </Modal>}
+          
+          
+//       </div>
+//   );
+// }
+//========================================================================================
 
 export default function App() {
-    const [articles, setArticles] = useState<Article[]>([]);
-    const [isLoading, setIsLoading] = useState(false);
-    // 1. Оголошуємо стан
-    const [isError, setIsError] = useState(false);
-
-    const handleSearch = async (topic: string) => {
-        // 2. Додаємо блок try...catch
-        try {
-            setIsLoading(true)
-             // 3. Скидаємо стан помилки в false перед кожним запитом
-            setIsError(false)
-            setArticles([]);
-            const data = await fetchArticles(topic);
-            setArticles(data)
-        } catch {
-            // 4. Встановлюємо стан isError в true
-            setIsError(true);
-        } finally {
-            // 5. Встановлюємо стан isLoading в false
-      // після будь якого результату запиту
-            setIsLoading(false);
+    const [clicks, setClicks] = useState(() => {
+        console.log("Initializer ran");
+        const saveClicks = localStorage.getItem("saveClicks");
+        	// Якщо там щось є, повертаємо це
+        // значення як початкове значення стану
+        if (saveClicks !== null) {
+            return JSON.parse(saveClicks)
         }
-  };
-
+        
+	// У протилежному випадку повертаємо
+        // яке-небудь значення за замовчуванням
+        return 0;
+    });
+    useEffect(() => {
+        localStorage.setItem("saveClicks", JSON.stringify(clicks))
+    }, [clicks])
   return (
-    <div>
-          <SearchForm onSubmit={handleSearch} />
-          {isLoading && <p>Loading data, please wait ...</p>}
-          {isError && <p>Whoops, something went wrong! Please try again!</p>}
-      {articles.length > 0 && <ArticleList items={articles} />}
-    </div>
+      <div>
+          <button onClick={() => setClicks(clicks + 1)}>You clicked {clicks} times</button>
+          <button onClick={()=> setClicks(0)}>Reset</button>
+      </div>
   );
 }
+//========================================================================================
+// export default function App() {
+//     const [clicks, setClicks] = useState(0);
+//     // 1. Тільки один раз після монтування
+//     useEffect(() => {
+//         console.log("You can see me only once!");
+//     }, [])
+//      // 2. Кожного разу, коли змінюється clicks
+//     useEffect(() => {
+//         console.log("Clicks updated:", clicks);
+//     }, [clicks])
+//      // 3. При кожному рендері (бо без залежностей)
+//     useEffect(() => {
+//         document.title = `You clicked ${clicks} times`
+//     });
+//   return (
+//       <button onClick={()=> setClicks(clicks +1)}>You clicks {clicks} times</button>
+//   );
+// }
+//========================================================================================
+
+
+// export default function App() {
+//     const [first, setFirst] = useState(0);
+//     const [second, setSecond] = useState(0);
+
+//     useEffect(() => {
+//         console.log("First updated:", first);
+//     }, [first])
+
+//    useEffect(() => {
+//         console.log("Second updated:", second);
+//    }, [second]) 
+    
+//    useEffect(() => {
+//         console.log("First or second updated:", first + second);
+//    }, [first, second]) 
+    
+//     return (
+//         <>
+//          <button onClick={() => setFirst(first + 1)}>First {first}</button>
+//       <button onClick={()=> setSecond(second +1)}>Second {second}</button></>
+     
+      
+//   );
+// }
+//========================================================================================
--- src/Components/OrderForm/OrderForm.tsx
+++ src/Components/OrderForm/OrderForm.tsx
@@ -1,15 +1,114 @@
-interface OrderFormProps{
-    onSubmit: (value: string) => void;
-}
-export default function OrderForm({ onSubmit }: OrderFormProps) {
-    const handeleSubmit = (formData: FormData) => {
-        const username = formData.get("username") as string;
-        onSubmit(username)
-    };
-  return (
-      <form action={handeleSubmit}>
-          <input type="text" name="username" />
-          <button type="submit">Place order</button>
+// interface OrderFormProps{
+//     onSubmit: (value: string) => void;
+// }
+//==========================================================================
+
+// export default function OrderForm({ onSubmit }: OrderFormProps) {
+//     const handeleSubmit = (formData: FormData) => {
+//         const username = formData.get("username") as string;
+//         onSubmit(username)
+//     };
+//   return (
+//       <form action={handeleSubmit}>
+//           <input type="text" name="username" />
+//           <button type="submit">Place order</button>
+//     </form>
+//   );
+// }
+
+
+//==========================================================================
+
+
+// import { useId } from 'react';
+// export default function OrderForm() {
+//     const filedId = useId();
+//     return (
+//     <form>
+//             <label htmlFor={`${filedId}-username`}>Name</label>
+//             <input type="text" name="username" id={`${filedId}-username`} />
+
+
+//              <label htmlFor={`${filedId}-email`}>Email</label>
+//             <input type="email" name="email" id={`${filedId}-email`} />
+//             <button type='submit'>Place order</button>
+//     </form>
+//     );
+// }
+//==========================================================================
+
+import { useId } from "react";
+
+export default function OrderForm() {
+    const selectId= useId();
+    const handleOrder = (formData: FormData) => {
+        const delyveryTime = formData.get("delyveryTime")as string;
+        console.log("Preferred delivery time: ", delyveryTime)
+//==========================================================================
+
+//         const delivery = formData.get("delivery") as string;
+//         console.log("🚀 ~ handleOrder ~ delivery:", delivery)
+//         const restrictions = formData.getAll("restrictions") as string[];
+//         console.log("🚀 ~ handleOrder ~ restrictions:", restrictions)
+};
+//   return (
+     
+//       <form action={handleOrder}>
+//           <fieldset>
+//               <legend>Delivery method:</legend>
+//               <label htmlFor="">
+//                   <input type="radio" name="delivery" value="pickup" defaultChecked />
+//                   Pickup
+//               </label>
+//               <label htmlFor="">
+//                   <input type="radio" name="delivery" value="courier"  />
+//                   Courier
+//               </label>
+//               <label htmlFor="">
+//                   <input type="radio" name="delivery" value="drone"  />
+//                   Drone delivery
+//               </label>
+//           </fieldset>
+//           <button type="submit">Pleace order</button>
+//           </form>
+    //       );
+//==========================================================================
+    
+    // return (
+    //     <form action={handleOrder}>
+    //         <fieldset>
+    //             <legend>Dietary restrictions:</legend>
+    //             <label>
+    //                 <input type="checkbox" name="restrictions" value="vegan" />
+    //                 Vegan
+    //             </label>
+    //             <label>
+    //                 <input type="checkbox" name="restrictions" value="gluten-free" />
+    //                 Gluten-free
+    //             </label>
+    //             <label>
+    //                 <input type="checkbox" name="restrictions" value="nut-free" />
+    //                 Nut-free
+    //             </label>
+    //         </fieldset>
+    //         <button type="submit">Submit</button>
+    
+    // </form>
+    // );
+//==========================================================================
+
+    return (
+        <form action={handleOrder}>
+            <label htmlFor={selectId}>Preferred delivery time</label>
+            <select name="delyveryTime" id={selectId} defaultValue="">
+                <option value="">-- Choose delivery time --</option>
+                <option value="morning">Morning (8:00–12:00)</option>
+                <option value="afternoon">Afternoon (12:00–16:00)</option>
+                <option value="evening">Evening (16:00–20:00)</option>
+
+            </select>
+            <button type="submit">Pleace order</button>
     </form>
-  );
+    );
 }
+//==========================================================================
